d8f6e4c4cc22c0d6dff37a3ea7191b84181ac3ef
Amol Vyas <amol.vyas0630@gmail.com>
2026-02-15 17:20:35 +0530

    removed express backend code

--- backend/src/types/apiError.types.ts
+++
@@ -1,8 +0,0 @@
-export class ApiError extends Error {
-  statusCode: number;
-  constructor(statusCode: number, message: string) {
-    super(message ?? "An error occurred");
-    this.statusCode = statusCode;
-    Object.setPrototypeOf(this, ApiError.prototype);
-  }
-}
--- backend/src/utils/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from "express";
-
-export const asyncHandler =
-  (fn: any): RequestHandler =>
-  (req, res, next) => {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-  };
